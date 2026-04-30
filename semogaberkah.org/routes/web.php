c92347e0533e01aa3c3251934d4e4aa89e8b44fc
cosmicHunter322 <cosmichunterz2@gmail.com>
2026-04-30 18:02:56 +0700

    bursa soal homepage

--- semogaberkah.org/routes/web.php
+++ semogaberkah.org/routes/web.php
@@ -8,4 +8,10 @@ Route::get('/our-team',[MainController::class, 'ourTeam']);
 Route::get('/login', function(){
     return view('login');
 });
+Route::get('/BursaSoal', function(){
+    return view('BursaSoal');
+});
+Route::get('/viewer', function () {
+    return view('viewer');
+});
 // Route::get('/bursa-soal',[MainController::class, 'bursaSoal']);
