e7cc9d70cf53b61bc20765effbe68f81010562f8
Irevle <elvincoding@gmail.com>
2026-05-01 13:43:05 +0700

    update dashboard & remove welcome

--- semogaberkah.org/routes/web.php
+++ semogaberkah.org/routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[MainController::class, 'homepage']);
-Route::get('/our-team',[MainController::class, 'ourTeam']);
+Route::get("/", [MainController::class, "homepage"]);
+Route::get("/our-team", [MainController::class, "ourTeam"]);
+Route::get("/dashboard", function () {
+    return view("dashboard");
+});
 // Route::get('/bursa-soal',[MainController::class, 'bursaSoal']);
